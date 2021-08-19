272a2fc9e78ef9a24cbe6d03627e8ebb1fb494b1
Aline Dias <martinsaline558@gmail.com>
2021-08-19 14:13:11 -0300

    algoritmo vizinho mais proximo

---
+++ algoritmoConstrutivo.py
@@ -0,0 +1,21 @@
+def vizinho_mais_proximo (grafo):
+    U = 0
+    C = []
+    Q = [ v for v in range (len (grafo)) ] #vertices a visitar
+    Q.remove(U) #remove o vértice u
+    C.append(U)
+    while len(Q) != 0:
+        distancia= float('inf')
+        for aresta in grafo(U):
+            vertice = aresta[0]
+            peso = aresta[1]
+
+            if peso <= distancia and vertice in Q: # se o peso da aresta for menor que distancia e o vertice pertencer a Q
+                distancia= peso
+                V= vertice #adjacente de menor distancia
+        C.append(V)
+        Q.remove(V)
+        U = V
+        C.append(C[0])
+    print (C)
+    return C
